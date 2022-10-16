d761fa47df9ed1c0c7b22b8a500f83cf25992f63
Stephan Creutz <stephan.cr@gmx.de>
2022-10-16 10:31:20 +0200

    read "interesting" tags

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,63 @@
 //! A simple example of parsing `.debug_info`.
 
-use object::{Object, ObjectSection};
-use std::{borrow, env, fs};
+#![warn(rust_2018_idioms)]
+#![warn(clippy::pedantic)]
 
-fn main() {
-    for path in env::args().skip(1) {
-        let file = fs::File::open(&path).unwrap();
-        let mmap = unsafe { memmap::Mmap::map(&file).unwrap() };
-        let object = object::File::parse(&*mmap).unwrap();
-        let endian = if object.is_little_endian() {
-            gimli::RunTimeEndian::Little
-        } else {
-            gimli::RunTimeEndian::Big
-        };
-        dump_file(&object, endian).unwrap();
+use gimli::read::{AttributeValue, EvaluationResult};
+use object::{Object, ObjectSection};
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::{borrow, env, error, fs};
+
+use clap::{Arg, Command};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let matches = Command::new(
+        Path::new(&env::args().next().unwrap())
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap(),
+    )
+    .version("0.1.0")
+    .arg(Arg::new("pe").takes_value(true))
+    .get_matches();
+
+    if !matches.is_present("pe") {
+        for path in env::args().skip(1) {
+            let file = fs::File::open(&path)?;
+            let mmap = unsafe { memmap::Mmap::map(&file)? };
+            let object = object::File::parse(&*mmap)?;
+            let endian = if object.is_little_endian() {
+                gimli::RunTimeEndian::Little
+            } else {
+                gimli::RunTimeEndian::Big
+            };
+            dump_file(&object, endian)?;
+        }
+    } else {
+        if let Some(ref value) = matches.value_of("pe") {
+            let file = fs::File::open(&value)?;
+            let mmap = unsafe { memmap::Mmap::map(&file)? };
+            let object = object::File::parse(&*mmap)?;
+            println!("{:?}", object.format());
+            let endian = if object.is_little_endian() {
+                gimli::RunTimeEndian::Little
+            } else {
+                gimli::RunTimeEndian::Big
+            };
+            dump_file(&object, endian)?;
+        }
     }
+
+    Ok(())
 }
 
-fn dump_file(object: &object::File, endian: gimli::RunTimeEndian) -> Result<(), gimli::Error> {
+fn dump_file(object: &object::File<'_>, endian: gimli::RunTimeEndian) -> Result<(), gimli::Error> {
     // Load a section and return as `Cow<[u8]>`.
-    let load_section = |id: gimli::SectionId| -> Result<borrow::Cow<[u8]>, gimli::Error> {
-        match object.section_by_name(id.name()) {
+    let load_section = |id: gimli::SectionId| -> Result<borrow::Cow<'_, [u8]>, gimli::Error> {
+        match object.section_by_name(dbg!(id.name())) {
             Some(ref section) => Ok(section
                 .uncompressed_data()
                 .unwrap_or(borrow::Cow::Borrowed(&[][..]))),
@@ -33,7 +70,7 @@ fn dump_file(object: &object::File, endian: gimli::RunTimeEndian) -> Result<(), 
 
     // Borrow a `Cow<[u8]>` to create an `EndianSlice`.
     let borrow_section: &dyn for<'a> Fn(
-        &'a borrow::Cow<[u8]>,
+        &'a borrow::Cow<'_, [u8]>,
     ) -> gimli::EndianSlice<'a, gimli::RunTimeEndian> =
         &|section| gimli::EndianSlice::new(&*section, endian);
 
@@ -42,26 +79,60 @@ fn dump_file(object: &object::File, endian: gimli::RunTimeEndian) -> Result<(), 
 
     // Iterate over the compilation units.
     let mut iter = dwarf.units();
+    let mut file = File::create("depth_deltas.dat")?;
     while let Some(header) = iter.next()? {
         println!(
-            "Unit at <.debug_info+0x{:x}>",
-            header.offset().as_debug_info_offset().unwrap().0
+            "Unit at <.debug_info+0x{:x}> version: {}",
+            header.offset().as_debug_info_offset().unwrap().0,
+            header.version()
         );
         let unit = dwarf.unit(header)?;
 
         // Iterate over the Debugging Information Entries (DIEs) in the unit.
         let mut depth = 0;
         let mut entries = unit.entries();
+        file.write_all(b"--\n")?;
         while let Some((delta_depth, entry)) = entries.next_dfs()? {
             depth += delta_depth;
+            file.write_fmt(format_args!("{} {}\n", delta_depth, depth))?;
             println!("<{}><{:x}> {}", depth, entry.offset().0, entry.tag());
 
             // Iterate over the attributes in the DIE.
             let mut attrs = entry.attrs();
             while let Some(attr) = attrs.next()? {
+                match attr.value() {
+                    AttributeValue::DebugStrRef(offset) => {
+                        println!(
+                            "offset = {} {:?}",
+                            offset.0,
+                            dwarf.string(offset)?.escape_ascii().to_string()
+                        );
+                    }
+                    AttributeValue::Exprloc(expr) => {
+                        let mut eval = expr.evaluation(unit.encoding());
+                        let result = match eval.evaluate()? {
+                            EvaluationResult::Complete => eval.result(),
+                            EvaluationResult::RequiresRelocatedAddress(addr) => {
+                                dbg!(eval.resume_with_relocated_address(addr)?);
+                                eval.result()
+                            }
+                            otherwise => vec![],
+                        };
+                        println!("{:?}", result);
+                    }
+                    AttributeValue::String(_) => {
+                        let x = dwarf.attr_string(&unit, attr.value())?;
+                        println!("string => {}", x.to_string()?);
+                    }
+                    AttributeValue::UnitRef(x) => {
+                        unit.entry(x)?;
+                    }
+                    _ => (),
+                }
                 println!("   {}: {:?}", attr.name(), attr.value());
             }
         }
     }
+
     Ok(())
 }
